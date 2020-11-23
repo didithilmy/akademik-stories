b6510d00a6cc98ddb8ea2d3429fc9dc81ccf6f2f
Muhammad Aditya Hilmy <mhilmy@hey.com>
2020-11-23 19:08:49 +0700

    Update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False),
+)
+
+# reading .env file
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,9 +34,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&h3fhpvuu$pj9@5th+&6jdw29l8dmacv^!ju7g^l!q(mc#z%d%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
